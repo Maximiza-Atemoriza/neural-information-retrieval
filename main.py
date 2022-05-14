9c98a8564c1e19298a2303ef88dc4a3dcb6578af
adrian13579 <adrianportales135@gmail.com>
2022-05-13 21:40:56 -0400

    Implement basic CLI with typer

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from parsers.cran_parser import parse_cran
 from information_retrieval import vector_model
-
-corpus = parse_cran()
-documents = [doc[".W"] for doc in corpus]
-
-documents = [ 'leon leon leon','leon leon leon zorro ', 'leon zorro nutria ', 'nutria' ]
-vectorizer, idf , tfidf = vector_model.documents_tfidf(documents) 
-query = 'what similarity laws must be obeyed when constructing aeroelastic models of heated high speed aircraft'
-query = 'nutria leon leon casa'
-query_vector = vector_model.query_tfidf(query,vectorizer, idf)
-
-print(vector_model.relevant_documents(query_vector,tfidf)[:10])
+import typer
 
 
+def main(dataset: str, ranking: int):
+    if "cran" == dataset.lower():
+        corpus = parse_cran()
+        documents = [doc[".W"] for doc in corpus]
+    else:
+        typer.echo(typer.style("Unknown dataset :(", fg=typer.colors.RED, bold=True))
+        exit(0)
+
+    typer.secho("Indexing documents...", fg=typer.colors.BLUE)
+    vectorizer, idf, tfidf = vector_model.documents_tfidf(documents)
+    typer.secho("Done!:)", fg=typer.colors.BLUE)
+
+    while True:
+        typer.secho("Enter query:", fg=typer.colors.BRIGHT_WHITE, bold=True)
+        query = input()
+        query = query.replace("\n", " ")
+        typer.echo(typer.style("Document ID:", fg=typer.colors.GREEN, bold=True))
+        query_vector = vector_model.query_tfidf(query, vectorizer, idf)
+        for doc_id, score in vector_model.relevant_documents(query_vector, tfidf)[
+            :ranking
+        ]:
+            print(doc_id)
 
 
-
-
+if __name__ == "__main__":
+    typer.run(main)
