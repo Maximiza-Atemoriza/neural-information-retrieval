b17223d1faaf92b25d410d87efb3ea1baba5438d
adrian13579 <adrianportales135@gmail.com>
2022-06-26 18:49:21 -0400

    Load GloVe embeddings

---
+++ src/embeddings/glove.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import numpy as np
+
+path_to_glove_file = './glove.6B.50d.txt'
+
+def load_glove():
+    # change cwd to import modules 
+    sys.path.insert(0, '')
+    actual_wd = os.getcwd()
+    os.chdir(os.path.dirname(__file__))
+
+    # parse embedding
+    embeddings_index = {}
+    with open(path_to_glove_file) as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+    print("Found %s word vectors." % len(embeddings_index))
+
+    # restore working directory 
+    os.chdir(actual_wd)
+
+    return embeddings_index
---
+++ src/embeddings/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def get_embedding_matrix(word_index, embeddings_index, num_tokens, embedding_dim):
+    hits = 0
+    misses = 0
+
+    # Prepare embedding matrix
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            # This includes the representation for "padding" and "OOV"
+            embedding_matrix[i] = embedding_vector
+            hits += 1
+        else:
+            misses += 1
+    print("Converted %d words (%d misses)" % (hits, misses))
+
+    return embedding_matrix
