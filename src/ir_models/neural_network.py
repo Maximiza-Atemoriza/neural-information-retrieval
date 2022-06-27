146328fb7757b86ed927c715c08d068ee07595be
adrian13579 <adrianportales135@gmail.com>
2022-06-26 22:59:12 -0400

    Define NetRank model

--- src/ir_models/neural_network.py
+++ src/ir_models/neural_network.py
@@ -4,7 +4,9 @@ from tensorflow import keras
 from ..embeddings.glove import load_glove
 from ..embeddings.utils import get_embedding_matrix
 from keras.layers import Embedding
-from ..utils import get_vectorizer, get_word_index
+from keras import layers
+from keras import Model
+from ..utils import get_vectorizer, get_word_index, get_training_dataset
 
 
 class NetRank:
@@ -32,4 +34,49 @@ class NetRank:
             trainable=False,
         )
 
-        # TODO: define nn model 
+        input_doc = keras.Input(shape=(None,), dtype="int64")
+        input_query = keras.Input(shape=(None,), dtype="int64")
+
+        embedded_sequences = embedding_layer(input_doc)
+        x = layers.Conv1D(128, 5, activation="relu")(embedded_sequences)
+        x = layers.MaxPooling1D(5)(x)
+        x = layers.Conv1D(128, 5, activation="relu")(x)
+        x = layers.MaxPooling1D(5)(x)
+        x = layers.Conv1D(128, 5, activation="relu")(x)
+        x = layers.GlobalMaxPooling1D()(x)
+        x = layers.Dense(4, activation="relu")(x)
+
+        embedded_sequences = embedding_layer(input_query)
+        y = layers.Conv1D(128, 5, activation="relu")(embedded_sequences)
+        y = layers.MaxPooling1D(5)(y)
+        y = layers.Conv1D(128, 5, activation="relu")(y)
+        y = layers.MaxPooling1D(5)(y)
+        y = layers.Conv1D(128, 5, activation="relu")(y)
+        y = layers.GlobalMaxPooling1D()(y)
+        y = layers.Dense(4, activation="relu")(y)
+
+        combined = layers.Concatenate()([x, y])
+
+        z = layers.Dense(4, activation="softmax")(combined)
+        self.model = Model(inputs=[input_doc, input_query], outputs=z)
+        self.model.summary()
+
+        X, Y = get_training_dataset(dataset)
+
+        docs = []
+        queries = []
+        for doc, query in X:
+            docs.append( self.vectorizer(doc).numpy())
+            queries.append( self.vectorizer(query).numpy())
+
+        docs = np.array(docs)
+        queries = np.array(queries)
+
+        score = np.array(Y)
+
+        self.model.compile(
+            loss=tf.keras.losses.MeanSquaredError(),
+            optimizer="rmsprop",
+            metrics=["acc"],
+        )
+        self.model.fit([docs, queries], score, batch_size=128, epochs=20)
