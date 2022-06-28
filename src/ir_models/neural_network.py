a7f2c01950313b8e347ef07f7fa07948a5f9ec3a
rodrigo-pino <rodrodpino@gmail.com>
2022-06-28 16:55:00 -0400

    Formatting

--- src/ir_models/neural_network.py
+++ src/ir_models/neural_network.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 from tensorflow import keras
 from ..embeddings.glove import load_glove
 from ..embeddings.utils import get_embedding_matrix
@@ -17,29 +16,28 @@ class NetRank:
         self.word_index = None
         self.model = None
 
-    def save(self,path,model_name) -> None:
-        real_path = path + '/' + 'net_rank' + model_name
+    def save(self, path, model_name) -> None:
+        real_path = path + "/" + "net_rank" + model_name
         self.model.save(real_path)
 
-        file = open(real_path + '_vectorizer','wb')
+        file = open(real_path + "_vectorizer", "wb")
         dill.dumps(self.vectorizer, file)
         file.close()
 
-        file = open(real_path + '_word_index','wb')
+        file = open(real_path + "_word_index", "wb")
         dill.dumps(self.word_index, file)
         file.close()
 
     def load(self, file_path, model_name) -> None:
         self.model = load_model(file_path + model_name)
 
-        file = open(file_path + model_name + '_vectorizer')
+        file = open(file_path + model_name + "_vectorizer")
         self.vectorizer = dill.load(file)
         file.close()
 
-        file = open(file_path + model_name + '_word_index')
+        file = open(file_path + model_name + "_word_index")
         self.word_index = dill.load(file)
         file.close()
-
 
     def train(self, dataset):
         self.vectorizer = get_vectorizer(dataset)
