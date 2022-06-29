13a4084a06a2f824c763649c1f018a0c27ab2ceb
adrian13579 <adrianportales135@gmail.com>
2022-06-29 09:42:45 -0400

    Add namedtuples for qrels, docs and queries

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,10 @@
 from .utils import remove_stopwords
 import dill
+from collections import namedtuple
+
+Query = namedtuple("Query","query_id text")
+Document = namedtuple("Document","doc_id text")
+Qrel = namedtuple("Qrel","query_id doc_id relevance")
 
 
 class IRDataset:
@@ -50,14 +55,14 @@ class IRDataset:
     def queries_iter(self):
         if self.queries is None:
             raise Exception("No dataset found!")
-        return self.queries.items()
+        return [Query(*item) for item in self.queries.items()]
 
     def docs_iter(self):
         if self.docs is None:
             raise Exception("No dataset found!")
-        return self.docs.items()
+        return [Document(*item) for item in self.docs.items()]
 
     def qrels_iter(self):
         if self.qrels is None:
             raise Exception("No dataset found!")
-        return self.qrels
+        return [Qrel(*item) for item in self.qrels]
